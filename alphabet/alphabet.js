d7c8bf014118506c112c282b3e6d7339744c4ea6
fabienhinault <fabienhinault@users.noreply.github.com>
2015-07-12 22:06:55 +0200

    Update alphabet.js

--- alphabet/alphabet.js
+++ alphabet/alphabet.js
@@ -1,81 +1,139 @@
-"use strict";
+(function () {
+  'use strict';
 
-var LetterState = function(letter, audioId){
-  this.letter = letter;
-  this.audioId = audioId;
-}
+  var letterStates = [],
+    letter,
+    i,
+    current,
+    letterLine = document.getElementById("letters"),
+    hint,
+    colors = ['#EC438E', '#50B1D8', '#1CCF2A', '#EF394B', '#F08F07',
+              '#F4EA39', '#AB00D8']; //, '#FF6801', '#EC438E', '', '', ''];
 
-var LetterErrorState = function(letter, audioId, next){
-  this.letter = letter;
-  this.audioId = audioId;
-  this.next = next;
-}
-
-var letterStates = [];
-
-for(var i=0; i<26; i++){
-  var letter =  String.fromCharCode(97 + i);
-  letterStates[i] = new LetterState(letter, "audio_" + letter);
-}
-
-for(var i=1; i<letterStates.length; i++){
-  letterStates[i-1].next = letterStates[i];
-}
-letterStates[letterStates.length-1].next = letterStates[0];
-
-var current = letterStates[0];
-var letterLine = document.getElementById("letters");
-var hint;
-
-var fixLetterLine = function(){
-  while(letterLine.scrollWidth > letterLine.offsetWidth){
-    letterLine.innerHTML = letterLine.innerHTML.substring(1);
+  function LetterState(letter, audioId) {
+    this.letter = letter;
+    this.audioId = audioId;
   }
-}
-LetterState.prototype.onRightKeyPress = function(){
-  document.getElementById(this.audioId).play();
-  // assert this === current;
-  current = this.next;
-  letterLine.innerHTML = letterLine.innerHTML + this.letter.toUpperCase() + " ";
-  fixLetterLine();
-};
 
-LetterState.prototype.onWrongKeyPress = function(){
-  hint = document.createElement("span");
-  hint.id = "hint";
-  hint.style.color = "LightGray";
-  var hintText = document.createTextNode(this.letter.toUpperCase());
-  hint.appendChild(hintText);
-  letterLine.appendChild(hint);
-  fixLetterLine();
-  current = new LetterErrorState(this.letter, this.audioId, this.next);
-}
-
-LetterState.prototype.onKeyPress = function(e){
-  if(this.letter == (e.key || String.fromCharCode(e.keyCode))){
-    this.onRightKeyPress();
-  }else{
-    this.onWrongKeyPress();
+  function LetterErrorState(letter, audioId, next) {
+    this.letter = letter;
+    this.audioId = audioId;
+    this.next = next;
   }
-};
 
-LetterErrorState.prototype = Object.create(LetterState.prototype);
+  for (i = 0; i < 26; i = i + 1) {
+    letter = String.fromCharCode(97 + i);
+    letterStates[i] = new LetterState(letter, "audio_" + letter);
+  }
+  for (i = 1; i < letterStates.length;  i = i + 1) {
+    letterStates[i - 1].next = letterStates[i];
+  }
+  letterStates[letterStates.length - 1].next = letterStates[0];
 
-LetterErrorState.prototype.onRightKeyPress = function(){
-  letterLine.removeChild(document.getElementById("hint"));
-  LetterState.prototype.onRightKeyPress.call(this);
-}
-
-LetterErrorState.prototype.onWrongKeyPress = function(){
-  hint.style.color = "gray";
-}
-
-var doKeyDown = function(e){
-  current.onKeyPress(e);
-};
-
-window.addEventListener( "keydown", doKeyDown, false );
-document.getElementById("restart").onclick = function(){
-  letterLine.innerHTML = "";
   current = letterStates[0];
-}
+
+  function fixLetterLine() {
+    while (letterLine.scrollWidth > letterLine.offsetWidth) {
+      letterLine.innerHTML = letterLine.innerHTML.substring(1);
+    }
+  }
+
+  
+  
+  LetterState.prototype.onRightLetter = function () {
+    document.getElementById(this.audioId).play();
+    // assert this === current;
+    current = this.next;
+    letterLine.innerHTML = letterLine.innerHTML + '<span style="color:' +
+      _.sample(colors) + '">' + this.letter.toUpperCase() + '</span>' +
+      " ";
+    fixLetterLine();
+  };
+
+  LetterState.prototype.onWrongLetter = function () {
+    hint = document.createElement("span");
+    hint.id = "hint";
+    hint.style.color = "LightGray";
+    var hintText = document.createTextNode(this.letter.toUpperCase());
+    hint.appendChild(hintText);
+    letterLine.appendChild(hint);
+    fixLetterLine();
+    current = new LetterErrorState(this.letter, this.audioId, this.next);
+  };
+
+  LetterState.prototype.onLetter = function (letter) {
+    if (this.letter ===  letter) {
+      this.onRightLetter();
+    } else {
+      this.onWrongLetter();
+    }
+  };
+
+  LetterErrorState.prototype = Object.create(LetterState.prototype);
+
+  LetterErrorState.prototype.onRightLetter = function () {
+    letterLine.removeChild(document.getElementById("hint"));
+    LetterState.prototype.onRightLetter.call(this);
+  };
+
+  LetterErrorState.prototype.onWrongLetter = function () {
+    hint.style.color = "gray";
+  };
+
+  function onKey(e) {
+    current.onLetter(e.key || String.fromCharCode(e.keyCode));
+  }
+  function onLetter(letter) {
+    current.onLetter(letter);
+  }
+
+  window.addEventListener("keypress", onKey, false);
+  
+  document.getElementById("restart").onclick = function () {
+    letterLine.innerHTML = "";
+    current = letterStates[0];
+  };
+  
+  function setOnKeyClick() {
+    [].forEach.call(document.getElementById("keys").children,
+      function (key) {
+        var letter = key.innerHTML.substring(0, 1).toLowerCase();
+        key.onclick = function (e) {
+          onLetter(letter);
+        };
+      }
+      );
+  }
+  setOnKeyClick();
+  
+  document.getElementById("swap").onclick = function () {
+    var i = Math.floor(Math.random() * 26),
+      key = document.getElementById("keys").children.item(i);
+    key.parentNode.insertBefore(key.nextSibling, key);
+  };
+  
+  document.getElementById("ordered").onclick = function () {
+    document.getElementById("keys").innerHTML =
+      _.map(_.range(26),
+            function (i) {
+          return '<div class="key">' + String.fromCharCode(i + 65) + "</div>";
+        }
+           ).join("");
+    setOnKeyClick();
+  };
+  document.getElementById("ordered").onclick();
+  
+  document.getElementById("shuffle").onclick = function () {
+    for (i = 0; i < 20; i = i + 1) {
+      document.getElementById("swap").onclick();
+    }
+  };
+  
+//  document.getElementById("shuffle").onclick = function () {
+//    for (i = 0; i < 26; i = i + 1) {
+//      var keys = document.getElementById("keys");
+//      keys.insertBefore(keys.children.item(i), _.sample(keys.children));
+//    }
+//  };
+  
+}());
