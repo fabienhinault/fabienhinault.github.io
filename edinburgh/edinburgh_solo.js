47833d2a41b871c01277c864eccc1f0e9178d2ae
fabienhinault <fabienhinault@users.noreply.github.com>
2024-07-19 14:22:54 +0200

    Update edinburgh_solo.js

--- edinburgh/edinburgh_solo.js
+++ edinburgh/edinburgh_solo.js
@@ -224,17 +224,48 @@ function probablePlayer(player) {
     return (unsure + player) / 2;
 }
 
-function enlargeVertices() {
-    document.querySelectorAll('g.node').forEach(g => {
-        // all vertices seem to be at least 18 away from each other.
-        const extraRadius = 9;
-        const ellipse = g.querySelector('ellipse');
-        const clonedEllipse = ellipse.cloneNode(false);
-        clonedEllipse.removeAttribute('stroke');
-        clonedEllipse.setAttribute('rx', Number(clonedEllipse.getAttribute('rx')) + extraRadius);
-        clonedEllipse.setAttribute('ry', Number(clonedEllipse.getAttribute('ry')) + extraRadius);
-        ellipse.insertAdjacentElement('beforebegin', clonedEllipse);
+function createSvgPoint(svg, vertex) {
+    let svgPoint = svg.createSVGPoint();
+    svgPoint.x = vertex.x;
+    svgPoint.y = vertex.y;
+    return svgPoint;
+}
+
+function createPolygon(svg, svgVertex, cell) {
+    const hes = cell.halfedges;
+    const firstHalfEdge = hes[0];
+    let polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    polygon.points.appendItem(createSvgPoint(svg, firstHalfEdge.getStartpoint()));
+    for (const halfEdge of hes) {
+        polygon.points.appendItem(createSvgPoint(svg, halfEdge.getEndpoint()));
+    }
+    if (!cell.closeMe) {
+        polygon.points.appendItem(createSvgPoint(svg, firstHalfEdge.getStartpoint()));
+    }
+    polygon.setAttribute('fill', 'white');
+    svgVertex.insertBefore(polygon, svgVertex.firstChild); 
+}
+
+function moveEdgesLast() {
+    document.querySelectorAll('#graph0 > g.edge').forEach(g => {
+        g.parentElement.appendChild(g);
     });
+}
+
+function voronoize() {
+    const box = document.querySelector('#graph0 > polygon');
+    const points = box.points;
+    let voronoi = new Voronoi();
+    const bbox = {xl: points[0].x, xr: points[2].x, yt: points[1].y, yb: points[0].y};
+    const vertices = [...document.querySelectorAll('#graph0 > g.node')];
+    let sites = vertices.map(g => g.querySelector('ellipse')).map(e => {return {x: Number(e.getAttribute('cx')), y: Number(e.getAttribute('cy'))};});
+    const diagram = voronoi.compute(sites, bbox);
+    const svg = box.closest('svg');
+    for (let iVertex = 0; iVertex < vertices.length; iVertex++) {
+        const voronoiId = sites[iVertex].voronoiId;
+        const cell = diagram.cells[voronoiId];
+        createPolygon(svg, vertices[iVertex], cell);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -246,11 +277,21 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     const game = new Game(nextss, JSON.parse(JSON.stringify(nextss)), new LocalStorageSequenceValueStorage(), new Clock(), onGameOver);
-    enlargeVertices();
+    moveEdgesLast();
+    voronoize();
 
     function getNodeId(node) {
         return node.id.substring(2);
     }
+
+    function getNodeCoordinates(nodeId) {
+        const svgNode = document.querySelector(`#id${nodeId} > ellipse`);
+        console.log(svgNode);
+    }
+
+    function getNodeSquaredDistance(nodeId, x, y) {}
+
+
 
     function play(tmp) {
         previous = current;
@@ -263,7 +304,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function updateSvgCurrentVertex() {
-        let ellipse = current.querySelector('ellipse + ellipse');
+        let ellipse = current.querySelector('ellipse');
         ellipse.setAttribute('fill', 'gray');
     }
 
@@ -275,7 +316,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 g.remove();
             }
         });
-        const lastCircle = previous.querySelector('ellipse + ellipse');
+        const lastCircle = previous.querySelector('ellipse');
         lastCircle.setAttribute('stroke', 'lightgray');
         lastCircle.setAttribute('fill', 'none');
         if (game.moves.length === 2) {
@@ -299,5 +340,4 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 });
-
 
